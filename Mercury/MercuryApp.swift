3efe296e58894282fe94639c88c8704b19a20df6
manorajesh <mano@raje.sh>
2023-06-13 16:52:19 -0700

    Initial Commit

---
+++ Mercury/MercuryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MercuryApp.swift
+//  Mercury
+//
+//  Created by Mano Rajesh on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct MercuryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
